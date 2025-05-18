33813136bb4fd9e26e04b9914ecd64ee009d7263
Mariëlle Korthout <marielle.korthout.jc@renesas.com>
2025-05-18 19:29:59 +0200

    Added copy_shape and new helper functions

--- Project/main/inc/helperFunctions.h
+++ Project/main/inc/helperFunctions.h
@@ -11,6 +11,8 @@
 #define native_loop()
 #endif
 
+void btnmatrix_in_to_out(uint8_t* btnmatrix_input, uint8_t* btnmatrix_output);
+void btnmatrix_to_ledmatrix(uint8_t* btnmatrix_shape, uint16_t* ledmatrix_shape);
 void clear_btnmatrix_leds();
 void show_correct();
 void show_incorrect();
