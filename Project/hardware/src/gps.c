ba49e75dce8c2bd7411b3baa58753738380d0837
Mariëlle Korthout <marielle.korthout.jc@renesas.com>
2025-06-08 11:57:14 +0200

    Testing improvement
    This adds Twister support, refactors some code to be testable and adds tests for mg7-10 and helperFunctions

--- Project/hardware/src/gps.c
+++ Project/hardware/src/gps.c
@@ -14,12 +14,14 @@ LOG_MODULE_REGISTER(gps, LOG_LEVEL_INF);
 
 struct gnss_data gpsData;
 
+#if CONFIG_GNSS_SATELLITES
 static void gnss_data_cb(const struct device *dev, const struct gnss_data *data)
 {
 	if (data->info.fix_status != GNSS_FIX_STATUS_NO_FIX) {
 		gpsData = *data;
 	}
 }
+#endif
 
 struct gnss_data getGnssData() {
 	return gpsData;
@@ -53,9 +55,9 @@ static void gnss_satellites_cb(const struct device *dev, const struct gnss_satel
 {
 	// LOG_INF("%s reported %u satellites!\r\n", dev->name, size);
 }
-#endif
 
 GNSS_SATELLITES_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_satellites_cb);
+#endif
 
 // Utility function for 
 // converting degrees to radians
