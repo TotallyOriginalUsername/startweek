bd1c1159ca49a6de7195b1e7cdabcfef6b933a27
LarsFaase <54939303+LarsFaase@users.noreply.github.com>
2025-06-10 16:12:27 +0200

    Update routegenerator.h

--- RoutesGenereren/routegenerator.h
+++ RoutesGenereren/routegenerator.h
@@ -1,25 +1,22 @@
+// routegenerator.h
+
+
 #ifndef ROUTEGENERATOR_H
 #define ROUTEGENERATOR_H
 
+#include <cstdint>
 #include <vector>
 
 struct Point {
-    float x, y;
-    float cost = 1.0f;
-
-    Point(float x, float y, float cost = 1.0f) : x(x), y(y), cost(cost) {}
-
-    bool operator==(const Point& other) const {
-        return x == other.x && y == other.y;
-    }
+    int64_t x;
+    int64_t y;
+    float   cost;
+    Point(int64_t x = 0, int64_t y = 0, float cost = 1.0f);
+    bool operator==(const Point& other) const;
 };
-
+extern std::vector<std::vector<int>>    routeIndices;
+extern std::vector<std::vector<Point>>  selectedPoints;
+extern std::vector<Point>               allPoints;
 void generate_routes();
-std::vector<std::vector<Point>> getGeneratedRoutes();
-std::vector<std::vector<int>> getGeneratedIndices();
-std::vector<Point> getAllPoints();
 
-extern std::vector<std::vector<int>> routeIndices;
-extern std::vector<std::vector<Point>> selectedPoints;
-extern std::vector<Point> allPoints;
 #endif // ROUTEGENERATOR_H
